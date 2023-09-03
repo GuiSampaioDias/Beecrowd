0fe32c79a1f8b03b0beecb1798d507e0a4e9d480
Guilherme Sampaio Dias <gui_sampaio@hotmail.com>
2023-09-03 08:52:55 -0300

    Primeiros códigos C# beecrowd

---
+++ C#/1001-Extremamente_Básico/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+ autor: Guilherme Sampaio Dias
+ exercício: 1001 Extremamente Básico
+ plataforma: Beecrowd
+ 
+ */
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int A = int.Parse(Console.ReadLine());
+        int B = int.Parse(Console.ReadLine());
+        int X = A + B;
+        Console.WriteLine("X = " + X);
+    }
+}
---
+++ C#/1002-Área_do_Círculo/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ autor: Guilherme Sampaio Dias
+ exercício: 1001 Extremamente Básico
+ plataforma: Beecrowd
+ 
+ */
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        double r = double.Parse(Console.ReadLine());
+        double p = 3.14159;
+        double quadrado = Math.Pow(r, 2);
+
+        double res = p * quadrado;
+        string res_formatado = res.ToString("f4");
+
+        Console.WriteLine("A=" + res_formatado);  
+
+    }
+}
