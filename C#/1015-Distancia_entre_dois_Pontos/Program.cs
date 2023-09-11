32b15e4bb63a5487408b66e487efad7b49861bd4
Guilherme Sampaio Dias <gui_sampaio@hotmail.com>
2023-09-10 21:29:48 -0300

    Beecrowd C# 10/09/2023

---
+++ C#/1015-Distancia_entre_dois_Pontos/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+autor: Guilherme Sampaio Dias
+exercício: 1015 - Distância Entre Dois Pontos
+plataforma: Beecrowd
+data: 10/09/2023
+Linguagem: C#
+
+*/
+using System;
+
+class Distancia_etre_dois_Pontos
+{
+    static void Main()
+    {
+        string pri_ponto = Console.ReadLine();
+        string[] primeiro = pri_ponto.Split(' ');
+        double.TryParse(primeiro[0], out double x1);
+        double.TryParse(primeiro[1], out double y1);
+
+        string seg_ponto = Console.ReadLine();
+        string[] segundo = seg_ponto.Split(' ');
+        double.TryParse(segundo[0], out double x2);
+        double.TryParse(segundo[1], out double y2);
+
+        double x = x2 - x1;
+        double x_quadrado = Math.Pow(x, 2);
+
+        double y = y2 - y1;
+        double y_quadrado = Math.Pow(y, 2);
+
+        double soma = x_quadrado + y_quadrado;
+
+        double distancia = Math.Sqrt(soma);
+        string dist_f = distancia.ToString("f4");
+        Console.WriteLine(dist_f);
+
+    }
+}
---
+++ C#/1036-Formula_de_Bhaskara/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+autor: Guilherme Sampaio Dias
+exercício: 1036 - Fórmula de Bhaskara
+plataforma: Beecrowd
+data: 10/09/2023
+Linguagem: C#
+
+*/
+using System;
+
+class Bhaskara
+{
+    static void Main()
+    {
+        string valores = Console.ReadLine();
+        string[] valor = valores.Split(' ');
+        double.TryParse(valor[0], out double A);
+        double.TryParse(valor[1], out double B);
+        double.TryParse(valor[2], out double C);
+
+        double delta = B * B - 4 * A * C;
+
+        if (2 * A == 0 || delta < 0)
+            Console.WriteLine("Impossivel calcular");
+
+        else
+        {
+            double raiz_delta = Math.Sqrt(delta);
+            double r1 = (-B + raiz_delta) / (2 * A);
+            double r2 = (-B - raiz_delta) / (2 * A);
+            string r1_f = r1.ToString("f5");
+            string r2_f = r2.ToString("f5");
+            Console.WriteLine("R1 = " + r1_f + "\nR2 = " + r2_f);
+        }
+    }
+}
