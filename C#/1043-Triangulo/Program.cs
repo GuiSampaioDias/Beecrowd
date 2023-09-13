81e117a0abba10d8cf77f85ba034312ea4845ff7
Guilherme Sampaio Dias <gui_sampaio@hotmail.com>
2023-09-12 22:49:16 -0300

    Beecrowd C# 12/09/2023

---
+++ C#/1043-Triangulo/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+autor: Guilherme Sampaio Dias
+exercício: 1043- Triângulo
+plataforma: Beecrowd
+data: 12/09/2023
+Linguagem: C#
+
+*/
+using System;
+
+public class Triangulo
+{
+    public static void Main()
+    {
+        string valores = Console.ReadLine();
+        string[] valor = valores.Split(' ');
+        double.TryParse(valor[0], out double A);
+        double.TryParse(valor[1], out double B);
+        double.TryParse(valor[2], out double C);
+
+        if (A + B > C && A + C > B && B + C > A)
+        {
+            double perimetro = A + B + C;
+            string perimetro_f = perimetro.ToString("f1");
+            Console.WriteLine("Perimetro = " + perimetro_f);
+        }
+        else
+        {
+            double area = (A + B) * C / 2;
+            string area_f = area.ToString("f1");
+            Console.WriteLine("Area = " + area_f);
+        }
+
+    }
+}
---
+++ C#/1044-Multiplos/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+autor: Guilherme Sampaio Dias
+exercício: 1044- Múltiplos
+plataforma: Beecrowd
+data: 12/09/2023
+Linguagem: C#
+
+*/
+using System;
+
+public class Multiplos
+{
+    public static void Main()
+    {
+        string valores = Console.ReadLine();
+        string[] valor = valores.Split(' ');
+        int.TryParse(valor[0], out int A);
+        int.TryParse(valor[1], out int B);
+
+        if (A % B == 0 || B % A == 0)
+            Console.WriteLine("Sao Multiplos");
+        else
+            Console.WriteLine("Nao sao Multiplos");
+    }
+}
